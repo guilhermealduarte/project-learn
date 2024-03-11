1169b67c2edad213298c4330593d0aee8e5c89ee
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-11 18:20:52 -0300

    Project Files

---
+++ src/main/java/com/guilhermeduarte/projectlearn/ProjectLearnApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeduarte.projectlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeduarte/projectlearn/ProjectLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeduarte.projectlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
